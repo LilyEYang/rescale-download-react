bc41df2fc413dd94d128c5354641b1a8f8d79aa2
Micah Feras <mferas@berkeley.edu>
2021-02-19 17:55:59 -0800

    feat: made a box and list

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 
-import { Panel } from "./styles";
+import { Panel, FileBox } from "./styles";
 
 class Download extends React.Component {
+  fileList = [
+    {
+      id: 1,
+      fileName: "file1.txt"
+    },
+    {
+      id: 2,
+      fileName: "data1.txt"
+    },
+    {
+      id: 3,
+      fileName: "file2.txt"
+    }];
+
+  listItems = this.fileList.map((file) =>
+  <li key={file.id}>
+    {file.fileName}
+  </li>
+  );
+
     render() {
         return (
           <div className="header">
-            <h1 className="Download-section"> DOWNLOAD </h1>
-            <Panel> 
-                <p> Hello </p>
+            <h1 className="Download-section"> DOWNLOAD FILES </h1>
+            <Panel>
+              <p>Selected Files</p>
+              <FileBox>
+                <ul>{this.listItems}</ul>
+              </FileBox>
             </Panel>
             <Panel>
                 <p> Hi </p>
--- src/components/Download/styles.ts
+++ src/components/Download/styles.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components' 
 
+
 export const Panel = styled.div`
-    height: 70%;
+    height: 100%;
     width: 45%;
     padding: 25px;
     display: inline-block;
     border: 2px solid black;
-`
+`;
+
+export const FileBox = styled.div`
+    width: 500px;
+    border: 0.5px solid grey;
+    padding: 20px;
+    margin: 40px;
+`;
