0d15141b8be2693490a6abe391ca9adb30172c85
Lily Yang <lily-yang@berkeley.edu>
2021-02-21 11:11:13 +1100

    feat(component): right panel style

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 
-import { Panel } from "./styles";
+import { Panel, Section } from "./styles";
+import Button from "./../Button/index";
 
 class Download extends React.Component {
     render() {
         return (
           <div className="header">
-            <h1 className="Download-section"> DOWNLOAD </h1>
-            <Panel> 
-                <p> Hello </p>
-            </Panel>
-            <Panel>
-                <p> Hi </p>
-            </Panel>
+            <Section>
+              <h1 className="download-title"> DOWNLOAD FILES FROM RESCALE </h1>
+              <div className="panels">
+                <Panel className="panel"> 
+                  <p> Hello </p>
+                </Panel>
+                <Panel className="panel">
+                  <p className="panelTitle"> LOCAL </p>
+                  <p className="panelSubheading"> SELECTED DOWNLOAD DESTINATION </p>
+                  <p className="downloadDest"> Users/Downloads/Rescale </p>
+                  <Button>SELECT DOWNLOAD DESTINATION</Button>
+                </Panel>
+              </div>
+            </Section>
           </div>
         );
     }
--- src/components/Download/styles.ts
+++ src/components/Download/styles.ts
@@ -1,9 +1,35 @@
 import styled from 'styled-components' 
 
 export const Panel = styled.div`
-    height: 70%;
-    width: 45%;
-    padding: 25px;
-    display: inline-block;
-    border: 2px solid black;
+    width: 45 vw;
+    height: 50 vh;
+    flex: 1 1 auto;
+    padding: 30px;
+    margin: 30px;
+
+    background: #FFFFFF;
+    border: 1px solid #939393;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+
+    .panelTitle {
+        font-family: Quicksand, sans-serif
+        font-style: normal;
+        font-weight: bold;
+        font-size: 24px;
+        line-height: 30px;
+        align-items: center;
+        letter-spacing: 0.03em;
+
+        color: #717171;
+
+    }
+`
+
+export const Section = styled.div`
+    margin: 0 auto;
+    .panels {
+        display: flex;
+    }
 `
